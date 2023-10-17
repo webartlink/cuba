13eefa7418adf78aef0636f8efb6178786f2e748
webartlink <linkartweb@gmail.com>
2023-10-17 11:27:26 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+
+$('.photo__slider').slick({
+
+	prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>',
+	nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>'
+});
+
+
+});
